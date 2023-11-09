cdcc5126c39a855a263e1f9a3a1bb7369362c6de
gaspypm <gaspypm@gmail.com>
2023-11-09 14:02:21 -0300

    Update

--- app.js
+++ app.js
@@ -178,7 +178,7 @@ function mostrarOpciones(respuesta1, respuesta2, respuesta3){
 }
 
 function populateQuestionDropdown(cantPreguntas) {
-  const selectQuestion = document.getElementById("numeroPregunta");
+  const selectQuestion = document.getElementById("selectQuestion");
   for (let i = 1; i <= cantPreguntas; i++) {
     const option = document.createElement("option");
     option.text = "Question " + i;
