5c023b24e8c5b96db26dcbdf33bbf21232da106d
gaspypm <gaspypm@gmail.com>
2023-11-10 11:03:04 -0300

    Update app.js

--- app.js
+++ app.js
@@ -193,7 +193,7 @@ function populateQuestionDropdown(cantPreguntas) {
   selectQuestion.addEventListener("change", function() {
     const selectedQuestion = localStorage.getItem("numPregunta");
     localStorage.setItem("numPregunta", selectedQuestion);
-    selectQuestion.value = localStorage.getItem("numPregunta");
+    selectQuestion.value = (int)(localStorage.getItem("numPregunta"));
     numeroPregunta.innerHTML = "Question " + selectedQuestion;
     main();
   });
