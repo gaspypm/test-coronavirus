a08a23f591eb60ac760011002c72f27117dd7cc6
gaspypm <gaspypm@gmail.com>
2023-11-12 07:55:45 -0300

    Revert "Update app.js"
    This reverts commit 5e107b692dfb13cf3c508b9fedb236ce7bb3555a.

--- app.js
+++ app.js
@@ -14,10 +14,7 @@ localStorage.setItem("numPregunta", 1);
 const id = localStorage.getItem("id");
 let cantPreguntas;
 const categoria = (id).slice(0, -3);
-
-
 console.log("ID:", id);
-
 switch (categoria){
   case "home":
     cantPreguntas = 33;
@@ -47,14 +44,12 @@ switch (categoria){
     alert("There was an error, try selecting the category again.")
 }
 document.getElementById("logo").style.display = 'block';
-
 for (let i = 1; i <= cantPreguntas; i++) {
   const option = document.createElement("option");
   option.text = "Question " + i;
   option.value = i;
   selectQuestion.add(option);
 }
-
 document.getElementById("prevQuestion").addEventListener('click', function(){
   let currentQuestion = parseInt(localStorage.getItem("numPregunta"));
   if (currentQuestion > 1) {
@@ -68,7 +63,6 @@ document.getElementById("prevQuestion").addEventListener('click', function(){
     main();
   }
 });
-
 document.getElementById("nextQuestion").addEventListener('click', function(){
   let currentQuestion = parseInt(localStorage.getItem("numPregunta"));
   if (currentQuestion < cantPreguntas) {
@@ -82,17 +76,14 @@ document.getElementById("nextQuestion").addEventListener('click', function(){
     main();
   }
 });
-
 async function obtenerArchivo(id){
   const archivo = await fetch("/questions/" + id.toString() + ".csv"); // Recibo el archivo con las preguntas y respuestas
   const datos = await archivo.text(); // Convierto el archivo a texto
   const tabla = datos.split("\n"); // Separo las preguntas
   return Promise.resolve(tabla);
 }
-
 function obtenerPreguntas(tabla){
   let preguntas = [];
-
   tabla.forEach(columna => {
     const fila = columna.split(";"); // Separo las preguntas de las respuestas
     const pregunta = fila[0];
@@ -100,16 +91,12 @@ function obtenerPreguntas(tabla){
     dificultades.push(pregunta.slice(-1));
   });
   preguntas.pop(); // Elimino el último elemento que debe estar vacío
-
   return preguntas; // Retorno el array modificado
 }
-
 function obtenerRespuestas(tabla){
   let respuestas = [];
-
   tabla.forEach(columna => {
     const fila = columna.split(";"); // Separo las preguntas de las respuestas
-
     for(let i = 1; i < 5; i++){ // Separo las respuestas
       if(fila[i] != undefined){
         const respuesta = fila[i];
@@ -120,146 +107,103 @@ function obtenerRespuestas(tabla){
   //console.log(respuestas);
   return respuestas;
 }
-
 function aleatorizarRespuestas(opciones){
   let seed = 42;
   Math.seedrandom(seed);
-
   for (let i = opciones.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [opciones[i], opciones[j]] = [opciones[j], opciones[i]];
   }
-
   return opciones;
 }
-
-function verificarCorrecta(opcion, correcta, respuesta1, respuesta2, respuesta3){
+function verificarCorrecta(respuesta, correcta, respuesta1, respuesta2, respuesta3){
   if(seleccionada == true){
     return;
   }
-  if(opcion.innerHTML != correcta){
+  if(respuesta.innerHTML != correcta){
     document.body.setAttribute('style', 'background-color: #2f308b');
-    opcion.style.background = "#D52444";
+    respuesta.style.background = "#D52444";
     console.log("The answer is incorrect.");
   }
-
   else {
     document.body.setAttribute('style', 'background-color: #2f308b');
-    opcion.style.background = "#008747";
+    respuesta.style.background = "#008747";
     console.log("The answer is correct.");
   }
-
   setTimeout(function(){ // Después de 1 segundo de elegir muestro si las otras opciones son correctas o no
     mostrarOpciones(respuesta1, respuesta2, respuesta3);
-    document.getElementById("fuente").style.display = 'block';
-    document.getElementById("fuente").style.opacity = '1';
   }, 1000);
+  seleccionada = true;
 }
-
 function mostrarOpciones(respuesta1, respuesta2, respuesta3){
-  if(respuesta1.innerHTML == correcta){
-    respuesta1.style.background = "#008747";
-    respuesta2.style.background = "#D52444";
-    respuesta3.style.background = "#D52444";
-  } else if(respuesta2.innerHTML == correcta){
-    respuesta1.style.background = "#D52444";
-    respuesta2.style.background = "#008747";
-    respuesta3.style.background = "#D52444";
-  } else if(respuesta3.innerHTML == correcta){
-    respuesta1.style.background = "#D52444";
-    respuesta2.style.background = "#D52444";
-    respuesta3.style.background = "#008747";
-  } else{
-    respuesta1.style.background = "#D52444";
-    respuesta2.style.background = "#D52444";
-    respuesta3.style.background = "#D52444";
-  }
-}
-
-selectQuestion.addEventListener("change", function() {
-    const selectedQuestion = this.value;
-    localStorage.setItem("numPregunta", selectedQuestion);
-    main();
-  });
-
-async function main(){
-  console.log("Entering main function");
-  const id = localStorage.getItem("id");
-
-  console.log("ID:", id);
-  let preguntas = [];
-  let respuestas = [];
-  let opciones = [];
-  let indicesRespuestas = [];
-  let j = 0;
-
-  for(let i = 0; i < cantPreguntas; i++){
-    indicesRespuestas[i] = i*3;
-  }
-
-  selectQuestion.value = localStorage.getItem("numPregunta");
-
-  let preguntaAleatoria = localStorage.getItem("numPregunta") - 1;
-  let indiceAleatorio = indicesRespuestas[0];
-
-  indiceRespuestaCorrecta.innerHTML = indiceAleatorio;
-  localStorage.setItem("numPregunta", preguntaAleatoria+1);
-  console.log("Question number:", preguntaAleatoria+1);
-
-  preguntas = obtenerPreguntas(await obtenerArchivo(id));
-  respuestas = obtenerRespuestas(await obtenerArchivo(id));
-
-  pregunta.innerHTML = preguntas[preguntaAleatoria];
-
-  correcta = respuestas[indicesRespuestas[preguntaAleatoria]];
-
-  for (let i = indicesRespuestas[preguntaAleatoria]; i < 105; i++){
-    if (respuestas[i].includes("\r")){
-      respuestas_span[j].innerHTML = respuestas[i];
-      opciones[j] = respuestas[i];
-      break;
-    }
-    else {
-      opciones[j] = respuestas[i];
-    }
-    j++;
-  }
-
-  opciones = aleatorizarRespuestas(opciones);
-
-  if (dificultades[preguntaAleatoria] >= 1 && dificultades[preguntaAleatoria] <= 3){
-      nivel.innerHTML = "Level " + (dificultades[preguntaAleatoria]).toString();
-      console.log("Level:", dificultades[preguntaAleatoria]);
-  }
-  else{
-      nivel.innerHTML = "Level 2";
-      console.log("ERROR: Level not found for question " + preguntaAleatoria.toString());
-  }
-
-  for (let k = 0; k < opciones.length; k++){
-    respuestas_span[k].style.display = 'block';
     respuestas_span[k].innerHTML = opciones[k];
   }
 
-  // verificarCorrecta(respuesta1, respuestas[indicesRespuestas[preguntaAleatoria]], respuesta1, respuesta2, respuesta3);
+  async function main(){
+    const id = localStorage.getItem("id");
+    console.log("ID:", id);
+    let preguntas = [];
+    let respuestas = [];
+    let opciones = [];
+    let indicesRespuestas = [];
+    let j = 0;
 
-  respuesta1.addEventListener('click', function(){
+    for(let i = 0; i < cantPreguntas; i++){
+      indicesRespuestas[i] = i*3;
+    }
+    
+    populateQuestionDropdown(cantPreguntas);
+
+    selectQuestion.value = localStorage.getItem("numPregunta");
+
+    let preguntaAleatoria = localStorage.getItem("numPregunta") - 1;
+
+    let indiceAleatorio = indicesRespuestas[0];
+    indiceRespuestaCorrecta.innerHTML = indiceAleatorio;
+
+    localStorage.setItem("numPregunta", preguntaAleatoria+1);
+    console.log("Question number:", preguntaAleatoria+1);
+
+    preguntas = obtenerPreguntas(await obtenerArchivo(id));
+    respuestas = obtenerRespuestas(await obtenerArchivo(id));
+
+    pregunta.innerHTML = preguntas[preguntaAleatoria];
+    correcta = respuestas[indicesRespuestas[preguntaAleatoria]];
+
+    for (let i = indicesRespuestas[preguntaAleatoria]; i < 105; i++){
+      if (respuestas[i].includes("\r")){
+        respuestas_span[j].innerHTML = respuestas[i];
+        opciones[j] = respuestas[i];
+        break;
+      }
+      else {
+        opciones[j] = respuestas[i];
+      }
+      j++;
+    }
+
+    opciones = aleatorizarRespuestas(opciones);
+
+    if (dificultades[preguntaAleatoria] >= 1 && dificultades[preguntaAleatoria] <= 3){
+        nivel.innerHTML = "Level " + (dificultades[preguntaAleatoria]).toString();
+        console.log("Level:", dificultades[preguntaAleatoria]);
+    }
+    else{
+        nivel.innerHTML = "Level 2";
+        console.log("ERROR: Level not found for question " + preguntaAleatoria.toString());
+    }
+    for (let k = 0; k < opciones.length; k++){
+      respuestas_span[k].style.display = 'block';
+      respuestas_span[k].innerHTML = opciones[k];
+    }
+
     verificarCorrecta(respuesta1, respuestas[indicesRespuestas[preguntaAleatoria]], respuesta1, respuesta2, respuesta3);
-    seleccionada = true;
-  })
-
-  respuesta2.addEventListener('click', function(){
     verificarCorrecta(respuesta2, respuestas[indicesRespuestas[preguntaAleatoria]], respuesta1, respuesta2, respuesta3);
-    seleccionada = true;
-  })
-
-  respuesta3.addEventListener('click', function(){
     verificarCorrecta(respuesta3, respuestas[indicesRespuestas[preguntaAleatoria]], respuesta1, respuesta2, respuesta3);
-    seleccionada = true;
-  })
+  
+  }
 
-}
+  document.addEventListener("DOMContentLoaded", function(event){
+    main();
+  });
 
-document.addEventListener("DOMContentLoaded", function(event){
-  main();
-});
